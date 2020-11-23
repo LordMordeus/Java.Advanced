adba69b51a3d2381daa643bd3cc4c76223645078
Vladimir Chechin <u3ybep@inbox.ru>
2020-11-23 20:07:00 +0700

    Задание 3

--- Lesson3/src/Phonebook.java
+++ Lesson3/src/Phonebook.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Phonebook {
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Phonebook {
+    HashMap<String, HashSet<String>> phoneBook;
+
+    public Phonebook() {
+        this.phoneBook = new HashMap<>();
+    }
+
+    public void addContact(String name, String phone) {
+        HashSet<String> book = phoneBook.getOrDefault(name, new HashSet<>());
+        book.add(phone);
+        phoneBook.put(name, book);
+    }
+
+    public void findAndPrint(String name) {
+        System.out.println("Contact " + name + " / Phone number " + phoneBook.getOrDefault(name, new HashSet<>()));
+    }
 }
